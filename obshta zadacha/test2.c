a6cb89f03649a55f4bc678789b1a7249903e075a
George Stoyanov <76865294+georgestoyanov06@users.noreply.github.com>
2021-01-23 14:36:28 +0200

    commit

---
+++ obshta zadacha/test2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+void itob();
+int main(void){
+    int n;
+    int b;
+    char s[64];
+    printf("Enter a whole number: ");
+    scanf("%d", &n);
+    printf("\nEnter a base for conversion: ");
+    scanf("%d", &b);
+    itob(n,s,b);
+    strrev(s);
+    printf("%s",s);
+    
+ }
+ void itob(int n,char s[],int b){
+     int i=0;
+     int c=0;
+     char m[64]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+     do{
+       i=n%b;
+       n/=b;
+       s[c]=m[i];
+       c++;
+    }    
+    while(n>0);
+    
+   
+ }

93884ea6955f3639213588b249fe71f5e3aa5848
George Stoyanov <76865294+georgestoyanov06@users.noreply.github.com>
2021-01-30 13:02:25 +0200

    commit

--- 20210118_15.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int nA=1;
-    printf("vuvedete chislo\n");
-    scanf("%d",&nA);
-    int nB=(nA == 2 ? 2 : 0 );/*pri nA=2 nB=2 pri vsqka druga stoinost na nA , nB=0*/
-    printf("A value is %d\n", nA); printf("B value is %d\n", nB);
-    
-}

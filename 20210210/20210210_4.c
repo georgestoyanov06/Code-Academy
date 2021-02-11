bd7375e676aba3886b74a93227dbe5557a25db4b
George Stoyanov <76865294+georgestoyanov06@users.noreply.github.com>
2021-02-11 14:43:46 +0200

    commit

---
+++ 20210210/20210210_4.c
@@ -0,0 +1,43 @@
+/*Напишете програма, която да генерира случайна парола от 6 букви
+докато user-а я хареса.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    
+    char choice=0;
+    int i;
+    int p;
+    
+   
+    char passSymbols[52]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char pass[7];
+    
+    
+    while(choice!='2'){
+        printf("if you want new password pres 1\n");
+        printf("for exit pres 2\n");
+        scanf("%c",&choice);
+       
+        if(choice=='1'){
+        
+            srand(time(NULL));
+            p=0;
+            while(p<6){
+            
+                i = rand()%52;
+                pass[p]=passSymbols[i];
+                p++;
+             
+             }
+            printf("Yor password is %s\n",pass);
+        
+            }
+        else if(choice=='2'){
+            break;
+        }
+       
+        
+    }
+}

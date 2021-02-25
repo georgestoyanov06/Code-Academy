073be94f80d4a105f5d917e379e5938e4790cc07
George Stoyanov <76865294+georgestoyanov06@users.noreply.github.com>
2021-02-25 17:08:59 +0200

    c

--- 20210224/20210224_12.c
+++ 20210224/20210224_12.c
@@ -16,3 +16,17 @@ return x > y ? x : y;
 
 опитвма се да намеря информация в интернет с която да я аправя, но не ми се получава ще кача останалите задачи и ще се опитам да направвя тази
 */
+#include <stdio.h>
+
+#define GENERIC_MAX(type)
+#define type type##_max(type x, type y)\
+{   return x > y ? x : y;}
+ GENERIC_MAX(int);
+
+int main(){
+   
+    int a=5;
+    int c=6;
+    int d= int_max(a,c);
+    printf("%d",d);
+}

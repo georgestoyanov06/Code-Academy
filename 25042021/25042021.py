e55b874f222fb9773f70ab9631541bab9577c5b0
George Stoyanov <76865294+georgestoyanov06@users.noreply.github.com>
2021-04-27 15:24:19 +0300

    Update 25042021.py

--- 25042021/25042021.py
+++ 25042021/25042021.py
@@ -1,17 +1,11 @@
-'''
-Created on Apr 27, 2021
-
-@author: Joro
-'''
-
-
-
 def my_sum1(a,b):
-    if type(a)== int and type(b)== str or type(b)==int and type(a)== str:
-            return a*b 
-    return a+b
+    if type(a) == int and type(b)== str or type(b)==int and type(a)== str:
+        print(a*b)  
+    else:
+        print(a+b)
 
 
-print(my_sum1(10, 35))
-print(my_sum1("hello", "there"))
-print(my_sum1('hello', 2))
+
+my_sum1(10,20)
+my_sum1("hello ", "there")
+my_sum1("hello", 2)
